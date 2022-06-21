47475cf034b7a23d269b6509e67031e85b394948
Norman Dankert <norman.dankert@outlook.com>
2022-06-21 18:42:10 +0000

    add some text content

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,35 @@
 		<div v-if="!loaded" class="overlay"></div>
 	</transition>
 
-	<div class="card">
-		<header>
+	<section class="card">
+		<header class="header">
 			<AppHeader />
 		</header>
 
-		<main class="main">Content goes here.</main>
+		<main class="main">
+			<p>
+				Hej visitor 👋<br />My name is Norman, though on the internet I go by
+				totkeks.
+			</p>
+			<p>
+				For professional inquiries you can find my resumé and contact me on
+				<a
+					class="textlink"
+					href="https://www.linkedin.com/in/norman-dankert/"
+					target="_blank"
+					>LinkedIn</a
+				>. I also prepared a
+				<a class="textlink" href="/job-preferences"
+					>special page for recruiters</a
+				>
+				that makes it easy to see if your offer matches my preferences.
+			</p>
+		</main>
 
-		<SocialLinks />
-	</div>
+		<footer class="social">
+			<SocialLinks />
+		</footer>
+	</section>
 
 	<footer class="footer">
 		<AppFooter />
@@ -31,6 +51,7 @@ onMounted(() => {
 <style lang="scss">
 #app {
 	display: grid;
+	row-gap: 1rem;
 
 	grid-template:
 		". .... ." 2fr
@@ -49,16 +70,36 @@ onMounted(() => {
 .card {
 	grid-area: main;
 	display: grid;
+	grid-template-columns: min-content;
 	grid-template-rows: auto;
-	row-gap: 2.5rem;
 
-	background-color: var(--background-color);
-	padding: 2.5rem;
-	border-radius: 0.5rem;
+	background-color: rgba(var(--background-color-rgb), 0.9);
+	padding: calc(var(--card-padding) / 2) var(--card-padding);
+	border-radius: var(--card-border-radius);
+}
+
+.header {
+	border-bottom: 1px solid rgba(var(--border-color-rgb), 0.3);
+	padding-bottom: calc(var(--card-padding) / 2);
+	margin-bottom: calc(var(--card-padding) / 2);
+}
+
+.social {
+	border-top: 1px solid rgba(var(--border-color-rgb), 0.3);
+	padding-top: calc(var(--card-padding) / 2);
+	margin-top: calc(var(--card-padding) / 2);
 }
 
 .main {
-	overflow-y: auto;
+	text-align: justify;
+
+	p {
+		font: 300 100% Museo Sans, sans-serif;
+
+		strong {
+			font-weight: 500;
+		}
+	}
 }
 
 .footer {
