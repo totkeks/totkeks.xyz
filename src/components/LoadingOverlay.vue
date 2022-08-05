0f5f760cb021d5fa6045964ad0fcee875f37ea3e
Norman Dankert <norman.dankert@outlook.com>
2022-08-05 20:10:50 +0000

    fix missing loading overlay

--- src/components/LoadingOverlay.vue
+++ src/components/LoadingOverlay.vue
@@ -19,6 +19,8 @@ onMounted(() => {
 </script>
 
 <style lang="scss" module>
+@use "@/styles/utils";
+
 .overlay {
 	display: block;
 	position: fixed;
@@ -28,7 +30,7 @@ onMounted(() => {
 	height: 100vh;
 	opacity: 1;
 	z-index: 1;
-	background: overlay(), var(--background-color);
+	background: utils.overlay(), var(--background-color);
 	pointer-events: none;
 
 	&-leave-active {
