244ce27335d9860e0554dffeb3c9e24693fa593d
Norman Dankert <norman.dankert@outlook.com>
2022-08-06 13:08:47 +0000

    use proper title for job page

--- src/router.ts
+++ src/router.ts
@@ -9,7 +9,7 @@ const routes: RouteRecordRaw[] = [
 	},
 	{
 		path: "/job-preferences",
-		name: "Job",
+		name: "Job Preferences",
 		component: () => import("@/pages/JobPreferencesPage.vue"),
 	},
 	{
